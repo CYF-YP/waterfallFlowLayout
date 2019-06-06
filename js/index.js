090374cef50bf372d5e44c6c4c914612615a5ae0
CYF <914621447@qq.com>
2019-06-06 11:00:28 +0800

    瀑布流1.6

--- js/index.js
+++ js/index.js
@@ -1,17 +1,49 @@
-window.onload = function () {
-    var wrap = document.getElementById('wrap');
+var pageData, pageNumber, waterfall;
 
+// 请求
+function getData(pageNumber) {
+    $.ajax({
+        url: "https://api.apiopen.top/musicBroadcasting",
+        type: 'GET',
+        dataType: "json",
+        data: {},
+        success: function (data) {
+            // 数据格式:[{src:xxx, href:xxx, text:xxx},{src:xxx, href:xxx, text:xxx}]
+            if (pageNumber == 1) {
+                for (x in data.result[0].channellist) {
+                    data.result[0].channellist[x].src = data.result[0].channellist[x].thumb;
+                    data.result[0].channellist[x].href = "www.baidu.com";
+                    data.result[0].channellist[x].text = data.result[0].channellist[x].name;
+                }
+                waterfall.data = waterfall.data.concat(data.result[0].channellist);
+                waterfall.wraplist(document.getElementById('wrap'), data.result[0].channellist);
+            }else {
+                for (x in data.result[1].channellist) {
+                    data.result[1].channellist[x].src = data.result[1].channellist[x].avatar;
+                    data.result[1].channellist[x].href = "www.baidu.com";
+                    data.result[1].channellist[x].text = data.result[1].channellist[x].name;
+                }
+                waterfall.data = waterfall.data.concat(data.result[1].channellist);
+                waterfall.wraplist(document.getElementById('wrap'), data.result[1].channellist);
+            }
+            // 组件中滚动总页数
+            pageData.totalPage = 3;
+        }
+    });
+}
+
+function init() {
     // 加载多页
-    var pageData = { pageSize: 10, totalPage: 1, dataDescription: "" };
+    pageData = { pageSize: 10, totalPage: 1, dataDescription: "" };
     // 组件使用
-    var waterfall = new waterfallFlowLayout({
+    waterfall = new waterfallFlowLayout({
         container: "#wrap",
         data: [],
         isScroll: true,
         pageData: pageData
     });
     // 监听页码改变
-    var pageNumber = 1;
+    pageNumber = 1;
     Object.defineProperty(pageData, "pageNumber", {
         get: function () {
             return pageNumber;
@@ -31,38 +63,11 @@ window.onload = function () {
             $("#descrip>span").text("已全部加载完毕!");
         }
     });
+}
 
-    // 请求
-    function getData(pageNumber) {
-        $.ajax({
-            url: "https://api.apiopen.top/musicBroadcasting",
-            type: 'GET',
-            dataType: "json",
-            data: {},
-            success: function (data) {
-                // 数据格式:[{src:xxx, href:xxx, text:xxx},{src:xxx, href:xxx, text:xxx}]
-                if (pageNumber == 1) {
-                    for (x in data.result[0].channellist) {
-                        data.result[0].channellist[x].src = data.result[0].channellist[x].thumb;
-                        data.result[0].channellist[x].href = "www.baidu.com";
-                        data.result[0].channellist[x].text = data.result[0].channellist[x].name;
-                    }
-                    waterfall.data = waterfall.data.concat(data.result[0].channellist);
-                    waterfall.wraplist(document.getElementById('wrap'), data.result[0].channellist);
-                }else {
-                    for (x in data.result[1].channellist) {
-                        data.result[1].channellist[x].src = data.result[1].channellist[x].avatar;
-                        data.result[1].channellist[x].href = "www.baidu.com";
-                        data.result[1].channellist[x].text = data.result[1].channellist[x].name;
-                    }
-                    waterfall.data = waterfall.data.concat(data.result[1].channellist);
-                    waterfall.wraplist(document.getElementById('wrap'), data.result[1].channellist);
-                }
-                // 组件中滚动总页数
-                pageData.totalPage = 3;
-            }
-        });
-    }
+window.onload = function () {
+    var wrap = document.getElementById('wrap');
+    init();
     getData(pageNumber);
 }
 
